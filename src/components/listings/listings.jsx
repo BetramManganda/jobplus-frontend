bdca3dcd0ec8270a2585e0175444a7153f2895c6
Betram Manganda <betrammanganda@hotmail.com>
2023-10-29 15:58:42 +0000

    paginate_update

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import './listings.scss';
+import React, { useState, useEffect } from "react";
+import "./listings.scss";
 
-import Paginate from '../paginate/paginate';
+import Paginate from "../paginate/paginate";
 
-import { StarSaved, Money, Location, Timer } from '../images';
+import { StarSaved, Money, Location, Timer } from "../images";
+import { useApi } from "../../hooks/useApi";
+
+const MAX_PER_PAGE = 3;
 
 export default function listings() {
+  const [jobs, setJobs] = useState([]);
+  const [meta, setMeta] = useState({});
+
+  const { get } = useApi();
+
+  const handleSuccess = (res) => {
+    const { entries, meta } = res.data;
+
+    setJobs(entries);
+    setMeta(meta);
+  };
+
+  const fetchJobs = async (page = 1) => {
+    await get("jobs", {
+      onSuccess: (res) => handleSuccess(res),
+      params: {
+        "populate[company]": true,
+        start: (page - 1) * MAX_PER_PAGE,
+        limit: MAX_PER_PAGE,
+      },
+    });
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return (
     <section>
-      <a href="apply.html">
-        <div class="listing__card">
-          <header class="listing__header">
-            <h1 class="listing__title">Regulatory Affairs Senior Manager</h1>
-            <img class="listing__saved" src={StarSaved} alt="" />
-            <p class="listing__company">
+      {jobs.map((job) => (
+        <div className="listing__card">
+          <header className="listing__header">
+            <h1 className="listing__title">{job.title}</h1>
+            <img className="listing__saved" src={StarSaved} alt="" />
+            <p className="listing__company">
               Posted by <span>Koco Media</span>
             </p>
           </header>
 
-          <ul class="listing__items">
+          <ul className="listing__items">
             <li>
               <img src={Money} alt="" />
               <b>Salary negotiable</b>
@@ -33,16 +62,15 @@ export default function listings() {
             </li>
           </ul>
 
-          <p class="listing__detail">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo,
-            asperiores ullam minus at <b>Read more...</b>
+          <p className="listing__detail">
+            {job.description} <b>Read more...</b>
           </p>
 
-          <a href="" class="listing__cta">
+          <a href="" className="listing__cta">
             Withdraw application
           </a>
         </div>
-      </a>
+      ))}
       <Paginate />
     </section>
   );
